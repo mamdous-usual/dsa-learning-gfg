af52eb6d563e1869da0890c02d485a81bc200b07
Mamdous <mamdouslaskar@protonmail.com>
2025-09-19 14:44:20 +0530

    add Check for Prime problem

---
+++ 02_Mathematics/Code/CheckPrime.java
@@ -0,0 +1,45 @@
+public class CheckPrime {
+    public static void main(String[] args) {
+
+        System.out.println(isPrimeNaive(11));
+        System.out.println(isPrimeEfficient(123));
+        System.out.println(isPrimeMoreEfficient(2));
+    }
+
+    // Naive Method
+    static boolean isPrimeNaive(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) { 
+                return false;
+            }
+        }
+        return true;
+}
+
+// Efficient Approach
+static boolean isPrimeEfficient(int n) {
+    if (n == 1) return false;
+    for (int i = 2; i*i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// More Efficient Approach
+static boolean isPrimeMoreEfficient(int n) {
+    if (n == 1) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i*i <= n; i = i+6) {
+        if (n % i == 0 || n % (i+2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+}
