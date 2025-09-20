760965383dc98b5e931fbd4838f8e8e9b0976f10
Mamdous <mamdouslaskar@protonmail.com>
2025-09-20 21:52:07 +0530

    Add Sieve of Ertosthenes problem

---
+++ 02_Mathematics/Code/Sieve.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+public class Sieve {
+    public static void main(String[] args) {
+        sieveSimple(10);
+        sieveOptimized(10);
+        sieveEfficient(10);
+    }
+
+    //  Simple Implementation of Sieve Algorithm
+    static void sieveSimple(int n) {
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i*i <= n; i++) {
+            if(isPrime[i]) {
+                for (int j = i*2; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i<=n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+     
+    //  Optimized Implementation of Sieve Algorithm
+    static void sieveOptimized(int n) {
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i*i <= n; i++) {
+            if(isPrime[i]) {
+                for (int j = i*i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i<=n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Shorter Implementation of Optimized Sieve Algorithm
+    static void sieveEfficient(int n) { // Time Complexity: O(nloglogn)
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime,true);
+        for (int i = 2; i<=n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+                for (int j = i*i; j <= n; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+ }
