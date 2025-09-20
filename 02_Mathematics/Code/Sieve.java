396e78ad09be3f59bd80a6c0c0c9263e1c6818f5
Mamdous <mamdouslaskar@protonmail.com>
2025-09-20 22:09:20 +0530

    Add gfg practice implementaion

--- 02_Mathematics/Code/Sieve.java
+++ 02_Mathematics/Code/Sieve.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+import java.util.*;
+
 public class Sieve {
     public static void main(String[] args) {
         sieveSimple(10);
@@ -58,4 +59,29 @@ public class Sieve {
             }
         }
     }
+    
+    // For GFG problem submission -> it wants integer array return type
+    static int[] sieve(int n) {
+    boolean[] isPrime = new boolean[n + 1];
+    Arrays.fill(isPrime, true);
+
+    ArrayList<Integer> primes = new ArrayList<>();
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            primes.add(i);
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    // Convert ArrayList<Integer> to int[]
+    int[] result = new int[primes.size()];
+    for (int i = 0; i < primes.size(); i++) {
+        result[i] = primes.get(i);
+    }
+    return result;
+}
+
  }
