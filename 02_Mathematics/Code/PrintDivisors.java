edd40c98bb985689e08fc9f8c1c1dce0d0b58ff0
Mamdous <mamdouslaskar@protonmail.com>
2025-09-20 14:31:15 +0530

    Fix PrintDivisors problem

--- 02_Mathematics/Code/PrintDivisors.java
+++ 02_Mathematics/Code/PrintDivisors.java
@@ -1,8 +1,12 @@
 public class PrintDivisors {
     public  static void main(String[] args) {
        printDivisorsNaive(15);
-       printDivisorsNotOrder(15);
-       printDivisorsEfficient(15);
+       /*
+         These two doesn't work they contain dubplicate. The lecture code was wrong;
+         printDivisorsNotOrder(15);
+         printDivisorsEfficient(20);
+       */
+        printDivisors(15);
     }
 
     // Naive Solution
@@ -14,6 +18,21 @@ public class PrintDivisors {
         }
     }
 
+    // Acceptable Efficient solution
+    static void printDivisors(int n) {
+        for (int i=1; i*i<n; i++) {
+            if (n%i == 0)
+                System.out.print(i + " ");
+        }
+        for (int i=(int)Math.sqrt(n); i>=1; i--) {
+            if (n%i == 0)
+                System.out.print(n/i + " ");
+        }
+    }
+
+/*
+ 
+// Warning!! -> it doesnot work for 20 , 100 etc there is always a dubplicate number
     // Efficient Solution -> not in order
     static void printDivisorsNotOrder(int n) { // Time Complexity: O(sqrt(n))
         for (int i = 1; i*i <= n; i++) { 
@@ -26,6 +45,7 @@ public class PrintDivisors {
         }
     }
     
+
     // Efficient Solution -> in order
     static void printDivisorsEfficient(int n) { // Time Complexity: O(sqrt(n))
         int i;
@@ -40,4 +60,6 @@ public class PrintDivisors {
             }
         }
     }
+*/
+
 }
