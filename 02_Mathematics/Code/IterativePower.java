f7b298880c31456dead014369df37ee6df2d6ef6
Mamdous <mamdouslaskar@protonmail.com>
2025-09-24 21:39:32 +0530

    Add Iterative pow(x,n) problem time complexity

--- 02_Mathematics/Code/IterativePower.java
+++ 02_Mathematics/Code/IterativePower.java
@@ -3,7 +3,7 @@ public class IterativePower {
        System.out.println(power(2,3));
     }
     // Iterative solution of pow(x,n)
-    static int power(int x, int n) {
+    static int power(int x, int n) { // Time Complexity:O(logn)
         int res = 1;
         while (n > 0) {
             if (n % 2 != 0) {
@@ -13,5 +13,10 @@ public class IterativePower {
             n = n/2;
         }
         return res;
+    }
+
+    // For the same problem in Leetcode -> have some edge cases
+    static int powerImproved(double x, int n) { // Time complexity: O(n)
+
     }
 }
