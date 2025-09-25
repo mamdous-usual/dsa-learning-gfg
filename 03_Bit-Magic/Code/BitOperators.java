f4038d8d883aa1ffe98341972507ce9c6d7b2dd3
Mamdous <mamdouslaskar@protonmail.com>
2025-09-25 22:02:55 +0530

    Correct typo in comment about bitwise operators

--- 03_Bit-Magic/Code/BitOperators.java
+++ 03_Bit-Magic/Code/BitOperators.java
@@ -1,6 +1,6 @@
 public class BitOperators {
     public static void main(String[] args) {
-        // Java has sever bitwise operators
+        // Java has seven bitwise operators
         int x = 3;
         int y = 6;
         System.out.println(x&y);// Bitwise AND(&)
