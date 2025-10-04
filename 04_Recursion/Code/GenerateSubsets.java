836a035e6cca7139ff39098fd114225d4cf7abe4
Mamdous <mamdouslaskar@protonmail.com>
2025-10-04 19:41:24 +0530

    Add Generate Subsets Problem

--- 04_Recursion/Code/GenerateSubsets.java
+++ 04_Recursion/Code/GenerateSubsets.java
@@ -1,5 +1,16 @@
 public class GenerateSubsets {
     public static void main(String[] args) {
+        subSets("ABC","",0);
       
+    }
+
+    static void subSets(String str, String current, int i) {
+        if (i == str.length()) {
+            System.out.println(current);
+            return;
+        }
+        subSets(str,current,i+1);
+        subSets(str,current+str.charAt(i),i+1);
+       
     }
 }
